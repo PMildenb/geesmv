7db16e10db93025288f01fc9339575d4d606ad88
Philipp Mildenberger <44525813+PMildenb@users.noreply.github.com>
2019-07-11 02:12:24 +0200

    now compatible with non-symmetric matrices
    the matrix to be inverted in the Kauermann-Carroll approach is (usually) not symmetric

--- R/mat.sqrt.R
+++ R/mat.sqrt.R
@@ -5,6 +5,8 @@ function(A)
    d<-ei$values
    d<-(d+abs(d))/2
    d2<-sqrt(d)
-   ans<-ei$vectors %*% diag(d2) %*% t(ei$vectors)
+   if(isSymmetric(A)) ei_vec_inv <- t(ei$vectors)
+   else               ei_vec_inv <- solve(ei$vectors)
+   ans <- ei$vectors %*% diag(sqrt(d)) %*% ei_vec_inv
    return(ans)
 }
